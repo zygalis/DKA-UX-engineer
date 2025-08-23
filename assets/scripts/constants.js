aec8134aa41c32412f926f46485cad29fb139648
Eerika Leppänen <eerika.leppanen@gmail.com>
2025-08-23 18:17:45 +0300

    first commit

---
+++ assets/scripts/constants.js
@@ -0,0 +1,5 @@
+export const statuses = [
+  { class: 'status-1', label: 'Status: Not Started' },
+  { class: 'status-2', label: 'Status: In Progress' },
+  { class: 'status-3', label: 'Status: Completed' },
+];
